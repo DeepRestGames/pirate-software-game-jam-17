9046ebd480ddc553028ab2223a10ed8a811d3989
enirfo <enrico.fornasa.it@gmail.com>
2025-07-31 11:58:15 +0200

    bomb explosion animation

--- if_onlyone_savemission/Scripts/Consumables/Bomb.gd
+++ if_onlyone_savemission/Scripts/Consumables/Bomb.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 @onready var animation_player = $AnimationPlayer
 @onready var hitbox_area = $HitboxArea
+@onready var sprite = $Sprite2D
+@onready var explosion_particles = $ExplosionParticles
 
 @export var bomb_damage = 3
 
@@ -12,8 +14,12 @@ func _ready() -> void:
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	explosion_particles.emitting = true
+	sprite.visible = false
 	if anim_name == "tick_boom":
 		for body in hitbox_area.get_overlapping_bodies():
 			body.take_damage(3)
 	
+
+func _on_explosion_particles_finished() -> void:
 	call_deferred("queue_free")
