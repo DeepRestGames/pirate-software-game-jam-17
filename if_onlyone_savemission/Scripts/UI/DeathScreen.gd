06abd3bd54775025372128c8476f4f40a69293cf
Marco Dugatto <dughi.boogie@gmail.com>
2025-07-31 13:35:51 +0200

    Clear map from enemies on player death

--- if_onlyone_savemission/Scripts/UI/DeathScreen.gd
+++ if_onlyone_savemission/Scripts/UI/DeathScreen.gd
@@ -14,6 +14,7 @@ func player_death() -> void:
 	
 	await fade_out_tween.finished
 	get_tree().get_first_node_in_group("Player").global_position = player_starting_position
+	EventBus.emit_signal("clear_map_from_enemies")
 	
 	var fade_in_tween = get_tree().create_tween()
 	fade_in_tween.tween_property(self, "modulate", Color(0.0, 0.0, 0.0, 0.0), .5)
